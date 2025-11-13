1e10ffd2a40ebd135d22a0e0943b71cd2d5d90fb
Chuseok22 <bjh59629@naver.com>
2025-11-13 15:35:49 +0900

    project init

---
+++ src/main/kotlin/com/chuseok22/roomreservationserver/RoomReservationServerApplication.kt
@@ -0,0 +1,11 @@
+package com.chuseok22.roomreservationserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoomReservationServerApplication
+
+fun main(args: Array<String>) {
+  runApplication<RoomReservationServerApplication>(*args)
+}
